5a36741bc9d73cd5232769a594a0a3d95fbf291b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-05 11:08:54 +0300

    functional update form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+// import Setup from './tutorial/1-useState/setup/5-useState-counter';
+import Setup from './tutorial/1-useState/setup/3-useState-array';
 
 function App() {
   return (
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,6 +7,15 @@ const UseStateCounter = () => {
     setValue(0)
   }
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    }, 2000)
+  }
+
   return <>
     <section style={{ margin: '4rem 0' }}>
       <h2>regular counter</h2>
@@ -20,6 +29,13 @@ const UseStateCounter = () => {
       {/*<button className="btn" onClick={() => setValue(0)}> */}
       <button className="btn" onClick={reset}>
         reset
+      </button>
+    </section>
+    <section style={{ margin: '4rem 0' }}>
+      <h2>more comples counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complexIncrease}>
+        increase later
       </button>
     </section>
   </>
