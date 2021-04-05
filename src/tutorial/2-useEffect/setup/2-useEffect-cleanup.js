1d37aabee1b4d468adbb862e266baa2920d64c2e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-05 14:23:37 +0300

    useEffect cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,28 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checksize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    console.log('useEffect')
+    window.addEventListener('resize', checksize)
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('resize', checksize)
+    }
+  }, []);
+
+  console.log('render')
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  )
 };
 
 export default UseEffectCleanup;
