2861967b67ff750c7839c9d2c93455c81f3d591b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-07 14:49:38 +0300

    add item to list (Form)

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,10 +9,20 @@ const ControlledInputs = () => {
 
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(firstName, email)
+    if (firstName && email) {
+      const person = { id:new Date().getTime().toString(), firstName, email }
+      setPeople((people) => {
+        return [...people, person]
+      });
+      setFirstName('')
+      setEmail('')
+    } else {
+      console.log('empty values')
+    }
   }
 
   return <>
@@ -26,13 +36,24 @@ const ControlledInputs = () => {
         </div>
         <div className="form-control">
           <label htmlFor="email">Email: </label>
-          <input type="text" id='email' name='email' value={email} 
+          <input type="text" id='email' name='email' value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <button type='submmit' onClick={handleSubmit}>add person</button>
       </form>
     </article>
+    <div>
+      {people.map((person, index) => {
+        const { id, firstName, email } = person
+        return (
+          <div className='item' key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        )
+      })}
+    </div>
   </>
 };
 
