e84d1bfc8f6cfac41891055f696e1b50e5227300
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-10 17:29:57 +0300

    Context API / useContext

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,45 +1,68 @@
 import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
+import { v4 as uuid } from "uuid";
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// two components - Provider, Consumer
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
+
   const removePerson = (id) => {
     setPeople((people) => {
       return people.filter((person) => person.id !== id);
     });
   };
+
+  const addPerson = () => {
+    const name = 'Lasse'
+    const newPerson = { id: uuid(), title: name };
+    if(people.find((person) => person.name === name)){
+      return
+    } else{
+      setPeople([...people, newPerson])
+    }
+  };
+
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{ removePerson, addPerson, people }}>
+      <h3>prop drilling -> Context API / useContext</h3>
+      <List />
+    </PersonContext.Provider >
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const mainData = useContext(PersonContext)
+  console.log(mainData)
   return (
-    <>
-      {people.map((person) => {
+    <div>
+      {mainData.people.map((person) => {
         return (
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
-    </>
+    </div>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext)
+  // const { addPerson } = useContext(PersonContext)
   return (
-    <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>remove</button>
-    </div>
+    <main>
+      <div className='item'>
+        <h4>{name}</h4>
+        <button onClick={() => removePerson(id)}>remove</button>
+        {/* <button onClick={() => addPerson()}>add</button> */}
+      </div>
+    </main>
+
   );
 };
 
