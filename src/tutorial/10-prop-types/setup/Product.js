a422d70da46f966b4bcc143f4e1c1c89e196423a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-12 17:58:15 +0300

    PropTypes - Default Values (images) - short circuit operators!

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -19,13 +19,14 @@ import PropTypes from 'prop-types';
 import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({ image, name, price }) => {
+  const url = image && image.url
   console.log(image, name, price)
   return (
     <article className='product'>
       {/* <h4>single product</h4> */}
-      <img src={image.url} alt={name} />
+      <img src={url || defaultImage} alt={name || 'default name'} />
       <h4>{name}</h4>
-      <p>${price}</p>
+      <p>${price || 3.99}</p>
     </article>
   )
 };
@@ -35,10 +36,11 @@ Product.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired
 }
-Product.defaultProps = {
-  name: 'default-name',
-  price: 3.99,
-  image: defaultImage
-}
+
+// Product.defaultProps = {
+//   name: 'default-name',
+//   price: 3.99,
+//   image: defaultImage
+// }
 
 export default Product;
